9b048615977d0b54b8155200c6fe287c5b4b1880
pinkskink1 <109717797+pinkskink1@users.noreply.github.com>
2022-07-21 16:33:38 +1000

    Update foobar.php
    add modulus functionality for the result required

--- foobar.php
+++ foobar.php
@@ -1,5 +1,20 @@
 #!/usr/local/bin/php -q
 <?php
-for ($i=0; $i<=100; $i++)
-    echo " $i, ";
+# Print out numbers 1 to 100
+# if divisible by 3 print foo
+# if divisible by 5 print bar
+# if divisible by both, print foobar
+
+for ($i=0; $i<=100; $i++){
+    if (($i%3 == 0) && ($i%5 == 0)){
+        echo " foobar";
+    }else if ($i%3 == 0){
+        echo " foo";
+    }else if ($i%5 == 0){
+        echo " bar";
+    }else{
+        echo " $i ";
+    }
+    if ($i < 100) echo ",";
+}
 ?>
