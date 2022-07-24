3cf7c5d19908a252db195960be2b7fe2406465e4
pinkskink1 <109717797+pinkskink1@users.noreply.github.com>
2022-07-24 19:45:00 +1000

    Final
    Finish adding read lines into database, tidy up

--- user_upload.php
+++ user_upload.php
@@ -1,9 +1,9 @@
 #!/usr/local/bin/php -q
 <?php
 
-/*
-* PRINT OUT HELP MANUAL
-*/
+//======================
+// PRINT OUT HELP 
+//=======================
 function print_help(){
 $message = <<<MES
     This program requires at least one of the following arguments:
@@ -20,14 +20,17 @@ $message = <<<MES
 MES;
 echo $message;
 }
-/*
-* Get rid of extraneous characters and set Camel Case
-*/
+//========================================
+// Get rid of extraneous characters 
+//(except single quote and dash) and set Camel Case
+//=================================================
 function clean_name($n){
 
     $n = preg_replace('/[^A-Za-z0-9\-\']/', '', $n); // Removes special chars.
     $n = ucfirst(strtolower($n));
+    
     // if e.g. O'hare, make H capital
+    
     $pos = strpos($n, "'");
     if($pos){
         $str1 = substr($n,$pos+1);
@@ -36,100 +39,82 @@ function clean_name($n){
     }
     return $n;
 }
-/*
-* Validate email and return good email or error
-*/
+//============================================
+//Validate email and return good email or null
+//==============================================
 function validate_email($email){
     $regex = '/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/';
     $email = strtolower($email);
     if (preg_match($regex, $email)) {
         return $email;
      } else {
-        return false;
+        echo "Email: ".$email." Is not valid.\n";
+        return null;
      }
 
 }
 
-/*
-*CREATE DATABASE TABLE
-*/
-function create_table($user,$password,$host,$sid){
+//=================================
+//CREATE DATABASE TABLE DROP first
+//================================
+function drop_create_table($user,$password,$host,$sid){
 try {
   $conn = new PDO("mysql:host=$host;dbname=$sid", $user, $password);
+  
     // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  
+  //drop the table
+  $sql = "DROP TABLE if EXISTS C_USERS";
+
+    $conn->exec($sql);
+    
+    echo "Table C_USERS dropped";
+
    //create table sql
   $sql = "CREATE TABLE C_USERS (
         id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-          firstname VARCHAR(30) NOT NULL,
-          lastname VARCHAR(30) NOT NULL,
-          email VARCHAR(50)
+          name VARCHAR(30) NOT NULL,
+          surname VARCHAR(30) NOT NULL,
+          email VARCHAR(50) ,
+          UNIQUE (email)
                       )";
 
     $conn->exec($sql);
-    echo "Table MyGuests created successfully";
+    
+    echo "Table C_USERS created successfully";
+    
    } catch(PDOException $e) {
                   echo $sql . "<br>" . $e->getMessage();
    }
 
-  $conn = null;
+  return $conn;
 }
-/*
-* REMOVE DATBASE TABLE
-*/
 
-function drop_table($user,$password,$host,$sid){
-try {
-  $conn = new PDO("mysql:host=$host;dbname=$sid", $user, $password);
-    // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-   //create table sql
-  $sql = "DROP TABLE C_USERS";
 
-    $conn->exec($sql);
-    echo "Table MyGuests created successfully";
-   } catch(PDOException $e) {
-                  echo $sql . "<br>" . $e->getMessage();
-   }
-
-  $conn = null;
-}
-function read_user_table($filename, &$users)
-{
-    $row = 0;
-
-    // read the data
-    $handle = fopen($filename, 'r');
-    if ($handle == 0)
-    return -1;
-
-    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-        $row++;
-        $num = count($data);
-        if (strlen($data[0]) != 0) {
-
-            $users[]     = $data[0];
-        }
-    }
-
-    fclose($handle);
-    return (count($users));
-}
 //========================================================================
 //  MAIN
 //========================================================================
+
+//initialise
+
 $dbusr = "";
 $dbpwd = "";
 $dbhost = "";
+$sid = "";
 $filename = "";
 $createtable = false;
 $dryrun = false;
 $help = false;
+
+
+//get arguments
+
 $dbopts = "u:p:h:";
 $otheropts = array("file:","create_table","dry_run","help");
 $otherrslt = getopt($dbopts,$otheropts);
-var_dump($otherrslt);
-    if(empty($otherrslt)) exit("Please use --help to find what parameters to use\n");
+//var_dump($otherrslt);
+if(empty($otherrslt)) exit("Please use \'--help\' to find what parameters to use\n");
 foreach ($otherrslt as $option => $value) {
 
         echo "{$option} => {$value} ";
@@ -146,6 +131,10 @@ foreach ($otherrslt as $option => $value) {
             case "h":
                 $dbhost = $value;
                 break;
+            case "s":
+                $sid = $value;  //Database name required for connection
+                break;
+
             case "create_table":
                 $createtable = true;
                 break;
@@ -162,32 +151,69 @@ foreach ($otherrslt as $option => $value) {
         }
     }
 
-//Drop and Create the Database Table
+//Drop and Create the Database Table (if not dry run)
 
+If (!$dryrun){
+
+	//make sure the dbparameters are filled before connection
+	
+	
+		$conn = drop_create_table(($user,$password,$host,$sid);
+		
+		// if only creating the table, stop here
+		If($createtable){
+			$conn = null;
+			exit();
+		}	
+		
+}
 
 
 //Open the file and add to database line by line ( rather than having a big array in memory)
 
-    $row = 0;
 
     // read the data
     $handle = fopen($filename, 'r');
+    
+    //wrong filename
     if ($handle == 0)
         exit("No file by that name exists!");
 
+	//read one line at a time
     while (($data = fgetcsv($handle, 0, "\t")) !== FALSE) {
-        $row++;
+        
         $num = count($data);
         if (strlen($data[0]) != 0) {
-
-            echo clean_name($data[0])." ".clean_name($data[1])."\n";
-            if(validate_email($data[2]))
-                    echo $data[2]."\n";
-
-        }
-    }
-
-
+        
+        //do validations, clean up
+        
+			$email = validate_email($data[2]);
+			$name = clean_name($data[0]);
+			$surname = clean_name($data[1]);
+			
+		//print out if doing a dry run	
+		if($dryrun){
+		
+			echo "Name: ".$name. " Surname: ".$surname. " Email: ".$email. "\n";
+		}
+			
+		//enter line into database
+		
+		If(!$dryrun && ($conn != null)){
+			$sql =’insert into C_USERS ( name, surname, email) values (:name, :surname, :email)’;
+			$statement = $conn->prepare($sql);
+			$statement->execute([
+				':name' => $name;
+				‘:surname’ => $surname;
+				‘:email’ =>  $email;
+			]);
+		}
+	}
+  
+    //Close Database Connection
+	$conn = null;
+	
+	//Close file reading
     fclose($handle);
 
 ?>
